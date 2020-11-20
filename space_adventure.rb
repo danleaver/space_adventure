f6683084118a1bce275f4d87ad299606605b4674
Dan Leaver <dleaver@gmail.com>
2020-11-19 18:37:27 -0700

    inital

---
+++ space_adventure.rb
@@ -0,0 +1,15 @@
+#user can choose some startup options
+#goes on adventure
+#there's an outcome
+
+#using classes
+#use a gem 'faker'
+#pushing to git 
+
+class Beginning
+end
+
+class Adventure
+end
+
+
