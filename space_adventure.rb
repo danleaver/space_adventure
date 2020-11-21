387f50be5deb93f58d77f1fb34add1e57902dd9b
Dan Leaver <dleaver@gmail.com>
2020-11-20 18:52:25 -0700

    more game logic

--- space_adventure.rb
+++ space_adventure.rb
@@ -25,17 +25,23 @@ class Beginning
       if choice == "yes"
         brings_dog = true
         dog_one = Dog.new
-        puts "DOG ONE"
-        puts dog_one.get_dogs_name
+        dog_info = dog_one.get_dogs_info
 
+        puts "Your dog is a #{dog_info[:size]} #{dog_info[:breed]} named #{dog_info[:name]}"
+        create_adventure(brings_dog, dog_info)
       elsif choice == "no"
         brings_dog = false
+        create_adventure(brings_dog)
       else 
         dog_question
       end
-
-      @adventure = Adventure.new(@user_name, brings_dog)
-      @adventure.your_adventure
+    end
+    
+  def create_adventure(brings_dog, dog_info)
+    @adventure = Adventure.new(@user_name, brings_dog)
+    value_returned = @adventure.your_adventure 
+    puts "your score is #{value_returned}"
+    puts brings_dog ? "thanks #{dog_info[:name]}" : "Better luck next time"
   end
 end
 
@@ -52,21 +58,35 @@ class Adventure
     puts "----------------------"
     puts 
     puts "Welcome #{@user_name}"
-    if @brings_dog === true
-    puts "you brought a dog"
-    else
-      puts "you didn't a bring a dog"
-    end
+
+    type_of_monster = Faker::Games::DnD.monster
+    puts "On your journey to outer space suddenly a giant #{type_of_monster} whose name is #{Faker::Name.name_with_middle}. "
+    
+    puts "press any key to continue"
+    gets
+    puts "the #{type_of_monster} attacks you."
+    
+    # if @brings_dog === true
+    #   puts "Fortunately your dog saves you. You Win."
+    # else
+    #   puts "You are killed by the monster. Maybe you should have brought a dog. GAME OVER."
+    # end
+    puts @brings_dog ? "Fortunately your dog saves you. You Win." : "You are killed by the monster. Maybe you should have brought a dog. GAME OVER."
+
+    random_number = rand(1..10)
+    # treasure
   end
 end
 
 class Dog 
   def initialize
     @your_dogs_name = Faker::Creature::Dog.name
+    @your_dogs_size = Faker::Creature::Dog.size
+    @your_dogs_breed =Faker::Creature::Dog.breed
   end
 
-  def get_dogs_name
-    @your_dogs_name 
+  def get_dogs_info
+    {name: @your_dogs_name, size: @your_dogs_size, breed: @your_dogs_breed} 
   end
 
 end
